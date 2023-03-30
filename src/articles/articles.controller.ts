a7a492a8a0cf0fb6430fea965e5c06d77992a2e6
Lone <adeyanju.akorede1@gmail.com>
2023-03-30 07:31:20 +0100

    feat: update swagger response types

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -9,9 +9,11 @@ import {
   ParseUUIDPipe,
   BadRequestException,
 } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
+import { ArticleEntity } from './entities/article.entity';
 
 const parseIDPipe = new ParseUUIDPipe({
   exceptionFactory(errors) {
@@ -20,32 +22,38 @@ const parseIDPipe = new ParseUUIDPipe({
 });
 
 @Controller('articles')
+@ApiTags('articles')
 export class ArticlesController {
   private;
 
   constructor(private readonly articlesService: ArticlesService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: ArticleEntity })
   create(@Body() createArticleDto: CreateArticleDto) {
     return this.articlesService.create(createArticleDto);
   }
 
-  @Get('drafts')
-  findDrafts() {
-    return this.articlesService.findDrafts();
-  }
-
   @Get()
+  @ApiOkResponse({ type: ArticleEntity, isArray: true })
   findAll() {
     return this.articlesService.findAll();
   }
 
+  @Get('drafts')
+  @ApiOkResponse({ type: ArticleEntity, isArray: true })
+  findDrafts() {
+    return this.articlesService.findDrafts();
+  }
+
   @Get(':id')
+  @ApiOkResponse({ type: ArticleEntity })
   findOne(@Param('id', parseIDPipe) id: string) {
     return this.articlesService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ type: ArticleEntity })
   update(
     @Param('id', parseIDPipe) id: string,
     @Body() updateArticleDto: UpdateArticleDto,
@@ -54,6 +62,7 @@ export class ArticlesController {
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: ArticleEntity })
   remove(@Param('id', parseIDPipe) id: string) {
     return this.articlesService.remove(id);
   }
--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -1,1 +1,24 @@
-export class Article {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Article } from "@prisma/client";
+export class ArticleEntity implements Article {
+    @ApiProperty()
+    id: string;
+
+    @ApiProperty()
+    title: string;
+
+    @ApiProperty({required: false, nullable: true})
+    description: string|null;
+
+    @ApiProperty()
+    body: string;
+
+    @ApiProperty()
+    published: boolean;
+
+    @ApiProperty()
+    created_at: Date;
+
+    @ApiProperty()
+    updated_at: Date;
+}
